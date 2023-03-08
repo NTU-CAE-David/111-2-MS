ca49c67d5aa9a299f8c7a4dbef0b94a301c19f86
NTU-CAE-David <r11521614@g.ntu.edu.tw>
2023-03-08 20:46:08 +0800

    調整函式的位置

--- HW_1_v1.0/ANGLE.h
+++ HW_1_v1.0/ANGLE.h
@@ -22,24 +22,28 @@ class ANGLE{
         double k0;      // Force constant in kcal/mol
         double theta0;  // Equilibrium angle in degrees
         double eng;     // angle energy in kcal/mol
+        double energy(double theta);
+        double potential_energy(double theta) const;
+        double force(double theta) const;
     
-        double energy(double theta) {
-            double rad_theta = theta * M_PI / 180.0;
-            double rad_theta0 = theta0 * M_PI / 180.0;
-            double cos_sin_delta = cos(rad_theta - rad_theta0) / sin(theta0);
-            eng = 0.5 * k0 * cos_sin_delta * cos_sin_delta;
-            return eng;
-        }
-        
-        double potential_energy(double theta) const {
-            double delta_theta = theta - theta0;
-            return 0.5 * k0 * delta_theta * delta_theta;
-        }
-
-        double force(double theta) const {
-            double delta_theta = theta - theta0;
-            return -k0 * delta_theta;
-        }
 };
+
+double ANGLE::energy(double theta) {
+    double rad_theta = theta * M_PI / 180.0;
+    double rad_theta0 = theta0 * M_PI / 180.0;
+    double cos_sin_delta = cos(rad_theta - rad_theta0) / sin(theta0);
+    eng = 0.5 * k0 * cos_sin_delta * cos_sin_delta;
+    return eng;
+}
+
+double ANGLE::potential_energy(double theta) const {
+    double delta_theta = theta - theta0;
+    return 0.5 * k0 * delta_theta * delta_theta;
+}
+
+double ANGLE::force(double theta) const {
+    double delta_theta = theta - theta0;
+    return -k0 * delta_theta;
+}
 
 #endif /* ANGLE_h */
