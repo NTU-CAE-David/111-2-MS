fbc4714ef167e9300c558dc0849e95eecf03a77b
NTU-CAE-David <r11521614@g.ntu.edu.tw>
2023-03-08 20:42:28 +0800

    HW_1 以header檔改進

---
+++ HW_1_v1.0/ANGLE.h
@@ -0,0 +1,45 @@
+//
+//  ANGLE.h
+//  HW_1_v1.0
+//
+//  Created by 陳啟瑋 on 2023/3/8.
+//
+
+#ifndef ANGLE_h
+#define ANGLE_h
+
+#include <iostream>
+#include <cmath>
+#include "ATOM.h"
+
+using namespace std;
+
+class ANGLE{
+    public:
+        ANGLE(){
+            k0=theta0=eng=0;
+        }
+        double k0;      // Force constant in kcal/mol
+        double theta0;  // Equilibrium angle in degrees
+        double eng;     // angle energy in kcal/mol
+    
+        double energy(double theta) {
+            double rad_theta = theta * M_PI / 180.0;
+            double rad_theta0 = theta0 * M_PI / 180.0;
+            double cos_sin_delta = cos(rad_theta - rad_theta0) / sin(theta0);
+            eng = 0.5 * k0 * cos_sin_delta * cos_sin_delta;
+            return eng;
+        }
+        
+        double potential_energy(double theta) const {
+            double delta_theta = theta - theta0;
+            return 0.5 * k0 * delta_theta * delta_theta;
+        }
+
+        double force(double theta) const {
+            double delta_theta = theta - theta0;
+            return -k0 * delta_theta;
+        }
+};
+
+#endif /* ANGLE_h */
