292966b6d7f3fcabdfd6179c34a248490f669274
NTU-CAE-David <r11521614@g.ntu.edu.tw>
2023-03-10 20:56:20 +0800

    Initial Commit

---
+++ HW_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW_1
+//
+//  Created by 陳啟瑋 on 2023/3/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
