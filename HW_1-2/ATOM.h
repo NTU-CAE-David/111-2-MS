888870754e97fc839e90798c42ad9d8f41cd1f2a
NTU-CAE-David <r11521614@g.ntu.edu.tw>
2023-03-10 21:32:31 +0800

    重新移植。

---
+++ HW_1-2/ATOM.h
@@ -0,0 +1,29 @@
+//
+//  ATOM.h
+//  HW_1
+//
+//  Created by 陳啟瑋 on 2023/3/10.
+//
+
+#ifndef ATOM_h
+#define ATOM_h
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class ATOM{
+    public:
+        ATOM(){
+            x[0]=x[1]=x[2]=chg=Ro=Do=0;
+        };
+    public:
+        double x[3]; //position vector of atom in angstroms
+        double chg; //partial charges in electrons
+        double Ro; //LJ Ro parameter in Angstroms
+        double Do; //LJ Do parameter in kcal/mol
+};
+
+#endif /* ATOM_h */
+
